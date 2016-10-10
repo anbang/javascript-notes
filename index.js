fe6661340177e5ae2f8d186fcbe072b49213ed99
zhuanbang <79228807@qq.com>
2016-10-10 19:17:39 +0800

    回家

--- index.js
+++ index.js
@@ -1,22 +1,7 @@
-/*
-//function curry(fn){
-function bind(fn,context){
-    //var args=Array.prototype.slice.call(arguments,1);
-    var args=Array.prototype.slice.call(arguments,2);
-    return function (){
-        var innerArgs=Array.prototype.slice.call(arguments);
-        var finnalArgs=args.concat(innerArgs);
-        console.log("args->"+args+"  innerArgs->"+innerArgs+"   finnalArgs->"+finnalArgs);
-        //return fn.apply(null,finnalArgs);
-        return fn.apply(context,finnalArgs);
-    }
-}
-
-var handler = {
-    message: "Event handled",
-    handleClick: function(name, event){
-        alert(this.message + ":"+ name + ":"+ event.type);
-    }
+var testObj={
+    name:"testObj",
+    age:"1"
 };
-var btn = document.getElementById("my-btn");
-EventUtil.addHandler(btn, "click", bind(handler.handleClick, handler, "my-btn"));*/
+console.log(Object.isExtensible(testObj)); //true
+Object.preventExtensions(testObj);
+console.log(Object.isExtensible(testObj)); //false
