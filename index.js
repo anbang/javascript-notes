7c3ba020b9d16f7d40fb43e2670be69df0a3499e
朱安邦的pro电脑 <broszhu@zhuanbangdeprodiannaodeMacBook-Pro.local>
2017-07-25 23:51:49 +0800

    MODI

--- index.js
+++
@@ -1,7 +0,0 @@
-var testObj={
-    name:"testObj",
-    age:"1"
-};
-console.log(Object.isExtensible(testObj)); //true
-Object.preventExtensions(testObj);
-console.log(Object.isExtensible(testObj)); //false
